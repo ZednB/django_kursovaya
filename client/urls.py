419352795fbeecc5c89a0169cf4e937ea066c429
Zubayr Bibulatov <Ru.Zubayr@gmail.com>
2024-03-05 22:20:12 +0300

    Настроил проект для начала работы

---
+++ client/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from client.apps import ClientConfig
+
+app_name = ClientConfig.name
+
+urlpatterns = [
+
+]
--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'client',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_kurs',
+        'USER': 'postgres',
+        'PASSWORD': '1111',
+        'HOST': '127.0.0.1',
+        'PORT': 5432,
     }
 }
 
@@ -116,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_FILES_DIRS = (
+    BASE_DIR / 'static',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
